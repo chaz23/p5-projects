8627c1f36dabfbf08dd638c850c392f0d48c6302
Charith Wijewardena <charithw01@gmail.com>
2024-11-30 22:18:58 +1100

    gen art 3 finished

--- projects/generative-art-3/shape.js
+++ projects/generative-art-3/shape.js
@@ -12,7 +12,6 @@ class Shape {
     this.y = {
       circle: y_,
       rect: y_ - squareLength / 2,
-      // triangle: [y_ + squareLength / 2, y_, y_ + squareLength / 2],
       triangle: [
         y_ - 2 * squareLength * sin(30) * cos(30),
         y_,
@@ -20,7 +19,7 @@ class Shape {
       ],
     };
 
-    this.color = getColor();
+    this.color = randomColor();
     this.hasFill = floor(random(0, 2));
     this.fill =
       this.hasFill === 0
@@ -32,52 +31,67 @@ class Shape {
         : stroke(this.color[0], this.color[1], this.color[2]);
   }
 
-  shapeSelector() {
-    const rand = floor(random(0, 5));
-    let shape;
-    const edgeCurve = 14;
+  circle() {
+    circle(this.x.circle, this.y.circle, squareLength);
+  }
 
-    if (rand === 0) {
-      shape = circle(this.x.circle, this.y.circle, squareLength);
-    } else if (rand === 1) {
-      shape = rect(this.x.rect, this.y.rect, squareLength, squareLength, 6);
-    } else if (rand === 2) {
-      shape = rect(
-        this.x.rect,
-        this.y.rect,
-        squareLength,
-        squareLength,
-        random([0, edgeCurve]),
-        random([0, edgeCurve]),
-        random([0, edgeCurve]),
-        random([0, edgeCurve])
-      );
-    } else if (rand === 3) {
-      push();
-      const offset = squareLength / 2;
-      rectMode(CENTER);
-      translate(this.x.rect + offset, this.y.rect + offset);
-      rotate(QUARTER_PI);
-      shape = rect(0, 0, squareLength / sqrt(2), squareLength / sqrt(2), 2);
-      pop();
-    } else {
-      shape = triangle(
-        this.x.triangle[0],
-        this.y.triangle[0],
-        this.x.triangle[1],
-        this.y.triangle[1],
-        this.x.triangle[2],
-        this.y.triangle[2]
-      );
-    }
-    return shape;
+  rect() {
+    rect(this.x.rect, this.y.rect, squareLength, squareLength, 6);
+  }
+
+  quad() {
+    const edgeCurve = 14;
+    rect(
+      this.x.rect,
+      this.y.rect,
+      squareLength,
+      squareLength,
+      random([0, edgeCurve]),
+      random([0, edgeCurve]),
+      random([0, edgeCurve]),
+      random([0, edgeCurve])
+    );
+  }
+
+  diamond() {
+    push();
+    const offset = squareLength / 2;
+    rectMode(CENTER);
+    translate(this.x.rect + offset, this.y.rect + offset);
+    rotate(QUARTER_PI);
+    rect(0, 0, squareLength / sqrt(2), squareLength / sqrt(2), 2);
+    pop();
+  }
+
+  triangle() {
+    triangle(
+      this.x.triangle[0],
+      this.y.triangle[0],
+      this.x.triangle[1],
+      this.y.triangle[1],
+      this.x.triangle[2],
+      this.y.triangle[2]
+    );
+  }
+
+  randomShape() {
+    const shapes = [
+      this.circle.bind(this),
+      this.rect.bind(this),
+      this.quad.bind(this),
+      this.diamond.bind(this),
+      this.triangle.bind(this),
+    ];
+
+    const rand = floor(random(0, shapes.length));
+    shapes[rand]();
   }
 
   render() {
     if (this.hasFill === 0) {
       circle(this.x.circle, this.y.circle, squareLength);
     } else {
-      this.shapeSelector();
+      this.randomShape();
     }
   }
 }
--- projects/generative-art-3/sketch.js
+++ projects/generative-art-3/sketch.js
@@ -1,35 +1,72 @@
 new p5();
 
-/* Constants. */
-const tilesPerSide = 20;
+const tilesPerSide = 30;
 
-/* Draw. */
 const canvasLength = windowWidth > windowHeight ? windowHeight : windowWidth;
 const squareLength = canvasLength / tilesPerSide;
 
-function getColor() {
+function randomColor() {
   const numColors = palette.length;
   const idx = floor(random(0, numColors));
   return palette[idx].hsb;
+}
+
+function getPixelIdx(x, y) {
+  const d = pixelDensity();
+  return 4 * d * (x + y * canvasLength);
 }
 
 function setup() {
   colorMode(HSB, 360, 100, 100);
   createCanvas(canvasLength, canvasLength);
   background(40, 30, 100);
-  noLoop();
-  randomSeed(123);
   strokeWeight(1.3);
+  noLoop();
 }
 
 function draw() {
   for (let x = 0; x <= tilesPerSide; x++) {
     for (let y = 0; y <= tilesPerSide; y++) {
-      noFill();
-      // strokeWeight(1);
-      // stroke("grey");
-      square(x * squareLength, y * squareLength, squareLength);
       new Shape(x * squareLength, y * squareLength).render();
+      noFill();
+      square(x * squareLength, y * squareLength, squareLength);
     }
   }
+
+  let offset = 0.1;
+  let y1 = Array(canvasLength);
+  let y2 = Array(canvasLength);
+
+  for (let x = 0; x < canvasLength; x++) {
+    y1[x] = canvasLength / 1 + map(noise(offset), 0, 1, 0, 250) - x / 2;
+    y2[x] = canvasLength / 0.85 + map(noise(offset), 0, 1, 0, 550) - x / 1.6;
+    offset += 0.0035;
+  }
+
+  y1 = y1.map((d) => floor(d));
+  y2 = y2.map((d) => floor(d));
+
+  loadPixels();
+  let colorPix = pixels;
+
+  filter(GRAY);
+
+  loadPixels();
+  let greyPix = pixels;
+
+  for (let x = 0; x < canvasLength; x++) {
+    for (let y = y1[x]; y < y2[x]; y++) {
+      const startIdx = getPixelIdx(x, y);
+
+      Array.from(
+        { length: 16 },
+        (d, i) => (colorPix[startIdx + i] = greyPix[startIdx + i])
+      );
+    }
+  }
+
+  for (let i = 0; i < pixels.length; i++) {
+    pixels[i] = colorPix[i];
+  }
+  updatePixels();
 }
